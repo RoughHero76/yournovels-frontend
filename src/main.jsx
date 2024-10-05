7acf248f945273e4ab96e547ff986d2ce79a8e71
Faizan Khan <m.faisalkhan9@gmail.com>
2024-10-05 13:15:24 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,39 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/logo512.png',
+        '/favicon.ico',
+      ])
+    }),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    }),
+  )
+})
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('service-worker registered: ', registration)
+      })
+      .catch((registrationError) => {
+        console.log('service-worker registration failed: ', registrationError)
+      })
+  })
+}
+
